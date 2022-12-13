78712708d10249741b5ec4a2a03c5f2d2c3bec89
SamenaBasith <115994259+SamenaBasith@users.noreply.github.com>
2022-12-13 14:23:51 +0000

    5 done

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -57,7 +57,6 @@ describe("/api/articles", () => {
       .get("/api/articles")
       .expect(200)
       .then((res) => {
-        console.log(res);
         const articles = res.body.articles;
         expect(articles).toHaveLength(12);
         articles.forEach((article) => {
@@ -75,7 +74,6 @@ describe("/api/articles", () => {
         });
       });
   });
-});
 
 test("status200 comment_count returns number of comments linked to each article", () => {
   return request(app)
@@ -96,7 +94,7 @@ test("status200 query accepted articles sorted by columns created_at and DESC or
       expect(articles).toBeSortedBy("created_at", { descending: true });
     });
 });
-test.only("status400 if bad request given: when invalid column given ", () => {
+test("status400 if bad request given: when invalid column given ", () => {
   return request(app)
     .get("/api/articles?sort_by=banana")
     .expect(400)
@@ -104,7 +102,7 @@ test.only("status400 if bad request given: when invalid column given ", () => {
       expect(msg).toBe("Bad Request");
     });
 });
-test("status400 if bad request given: when invalid order given ", () => {
+test("error status400 if bad request given: when invalid order given ", () => {
   return request(app)
     .get("/api/articles?order=banana")
     .expect(400)
@@ -112,3 +110,47 @@ test("status400 if bad request given: when invalid order given ", () => {
       expect(msg).toBe("Bad Request");
     });
 });
+})
+
+
+describe("/api/articles/:article_id", () => {
+    test("GET status200 returns an article object corressponding to the article_id passed", () => {
+      return request(app)
+        .get("/api/articles/1")
+        .expect(200)
+        .then((res) => {
+        const articles = res.body.article;
+          expect(articles).toEqual(
+            expect.objectContaining({
+              article_id: 1,
+              title: "Living in the shadow of a great man",
+              topic: "mitch",
+              author: "butter_bridge",
+              created_at: "2020-07-09T20:11:00.000Z",
+              votes:100
+            })
+          );
+        });
+      });
+  test("error status400 responds with bad request message when passed an invalid article_id such as letters instead of number", () => {
+    return request(app)
+      .get("/api/articles/banana")
+      .expect(400)
+      .then(({ body: { msg } }) => {
+        expect(msg).toBe("Bad Request: invalid input");
+      });
+    })
+      test("404: responds message 'not found'  when article doesnt exist", () => {
+        return request(app)
+          .get("/api/articles/123456")
+          .expect(404)
+          .then(({ body: { msg }}) => {
+            expect(msg).toBe("not found");
+          });
+      });
+})
+
+  
+
+  
+  
