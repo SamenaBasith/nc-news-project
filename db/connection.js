1792b7841d5a79b1d904f8958772c37fedaf43c2
SamenaBasith <115994259+SamenaBasith@users.noreply.github.com>
2023-01-04 14:16:39 +0000

    hosting

--- db/connection.js
+++ db/connection.js
@@ -8,7 +8,7 @@ require("dotenv").config({
 
 const config = {};
 if (ENV === "production") {
-  config.connectionString = process.env.DATABASE_URL,
+  config.connectionString = process.env.DATABASE_URL;
   config.max = 2;
 }
 
