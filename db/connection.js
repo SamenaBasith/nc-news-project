2184738b35b654dba06ab60c6a95acd6d441a13d
SamenaBasith <115994259+SamenaBasith@users.noreply.github.com>
2023-01-04 14:49:48 +0000

    hosting

--- db/connection.js
+++ db/connection.js
@@ -11,6 +11,7 @@ if (ENV === "production") {
   config.connectionString = process.env.DATABASE_URL;
   config.max = 2;
 }
+console.log(config)
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error("PGDATABASE not set or DATABASE_URL not set");
